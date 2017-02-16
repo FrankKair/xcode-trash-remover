268f20718d828614e864acd675993fda35b9f7f8
Frank Kair <frankkair@gmail.com>
2017-02-16 09:48:09 -0200

    Removes delete_trash_files method to use one function call

--- xcode_trash_remover.rb
+++ xcode_trash_remover.rb
@@ -2,38 +2,35 @@
 require 'fileutils'
 
 module Directory
-	DERIVED_DATA_DIR = "/Users/#{ARGV[0]}/Library/Developer/Xcode/DerivedData/*"
-	ARCHIVES_DIR = "/Users/#{ARGV[0]}/Library/Developer/Xcode/Archives/*"
+	DERIVED_DATA_DIRECTORY = "/Users/#{ARGV[0]}/Library/Developer/Xcode/DerivedData/*"
+	ARCHIVES_DIRECTORY = "/Users/#{ARGV[0]}/Library/Developer/Xcode/Archives/*"
 end
 
 def check_argv
 	if ARGV.empty?
 		puts 'No user path specified'
 		puts 'Usage: ./xcode_trash_remover.rb Username' 
+		puts 'The username is the one used in the path /Users/<username>/Library/Developer/Xcode'
 		abort
 	end
 end
 
-def check_folders
+def check_folders_and_delete_trash_files
 	Directory.constants.each do |dir|
 		trash_dir = Directory.const_get(dir)
 		files = Dir.glob(trash_dir)
 		if files.empty?
-			puts "There are no files in #{trash_dir}"
+			puts "There are no files in #{dir}"
+			next
 		end
+		puts "#{dir} files:"
 		files.each do |folder|
 			puts File.basename(folder)
+			FileUtils.rm_rf(folder)
 		end
-	end
-end
-
-def delete_trash_files
-	Directory.constants.each do |dir|
-		FileUtils.rm_rf(Dir.glob(Directory.const_get(dir)))
-		puts "Deleted all files from #{Directory.const_get(dir)}"
+		puts "Deleted all files from #{dir}"
 	end
 end
 
 check_argv
-check_folders
-delete_trash_files
+check_folders_and_delete_trash_files
