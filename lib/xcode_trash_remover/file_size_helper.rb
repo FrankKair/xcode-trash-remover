4aa5e8d54d4a95df78e843007c7ad91fca3cfc02
Frank Kair <frankkair@gmail.com>
2017-04-29 00:32:33 -0300

    Implements module function call with extend self

--- lib/xcode_trash_remover/file_size_helper.rb
+++ lib/xcode_trash_remover/file_size_helper.rb
@@ -1,21 +1,30 @@
 require 'fileutils'
 require 'find'
+
 module XcodeTrashRemover
 
-	def dir_size(dir_path)
-	  total_size = 0
-	  Find.find(dir_path) do |path|
-	      if FileTest.directory?(path)
-	        if File.basename(path)[0] == ?.
-	          Find.prune
-	        else
-	          next
-	        end
-	      else
-	        total_size += FileTest.size(path)
-	      end
-	    end
-	    total_size
+	module SizeHelper
+
+		extend self
+
+		def dir_size(dir_path)
+		  total_size = 0
+		  Find.find(dir_path) do |path|
+		      if FileTest.directory?(path)
+		        if File.basename(path)[0] == ?.
+		          Find.prune
+		        else
+		          next
+		        end
+		      else
+		        total_size += FileTest.size(path)
+		      end
+		    end
+		    total_size
+		end
+
+		# TODO: function to return best size (MB or GB)
+
 	end
 
 end
