6e1aae00b0d17147552b21eb611a80bf4a8e9e10
Frank Kair <frankkair@gmail.com>
2017-06-02 19:29:54 -0300

    Adds array with os' to delete from core simulator

--- lib/xcode_trash_remover/core.rb
+++ lib/xcode_trash_remover/core.rb
@@ -27,18 +27,22 @@ module XcodeTrashRemover
 
 		def get_core_simulator_folders
 			dirs = []
+
+			# TODO: Select which version to delete (8, 9, Watch, TV...)
+			os = ['iOS-8', 'iOS-9', 'watchOS', 'tvOS']
+
 			core_simulator_dir = "#{File.expand_path('~')}/Library/Developer/CoreSimulator/Devices/*/*"
+
 			Dir.glob(core_simulator_dir).each do |dir|
 				unless dir.include?('plist')
 					next
 				end
 				device_plist = "/#{dir}"[1..-1]
-				#  TODO: Select which version to delete (8, 9, Watch, TV...)
-				if File.read(device_plist).include?('iOS-8') or 
-					File.read(device_plist).include?('iOS-9') or 
-					File.read(device_plist).include?('watchOS') or 
-					File.read(device_plist).include?('tvOS')
-					dirs.push(File.dirname(dir))
+
+				os.each do |os|
+					if File.read(device_plist).include?(os)
+						dirs.push(File.dirname(dir))
+					end
 				end
 			end
 			dirs
